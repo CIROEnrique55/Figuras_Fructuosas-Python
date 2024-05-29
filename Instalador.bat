035f6b5ab9f800470ef8ceb8cca1a943a1162e70
CIROEnrique55 <ciroenrique4@hotmail.com>
2024-05-28 22:55:14 -0600

    Agregar todos los archivos de la carpeta

---
+++ Instalador.bat
@@ -0,0 +1,32 @@
+@echo off
+echo Instalando bibliotecas de Python...
+echo 
+echo 
+echo   .oooooo.    o8o             .oooooo.    .oooooo..o                     o8o            
+echo  d8P'  `Y8b   `"'            d8P'  `Y8b  d8P'    `Y8                     `"'            
+echo 888          oooo  oooo d8b 888      888 Y88bo.       .ooooo.  oooo d8b oooo  oo.ooooo. 
+echo 888          `888  `888""8P 888      888  `"Y8888o.  d88' `"Y8 `888""8P `888   888' `88b
+echo 888           888   888     888      888      `"Y88b 888        888      888   888   888
+echo `88b    ooo   888   888     `88b    d88' oo     .d8P 888   .o8  888      888   888   888
+echo  `Y8bood8P'  o888o d888b     `Y8bood8P'  8""88888P'  `Y8bod8P' d888b    o888o  888bod8P'
+echo                                                                                888      
+echo                                                                               o888o        
+
+pip install --upgrade pip
+pip install pyinstaller
+pip install nuitka
+pip install pygame
+pip install numpy
+pip install pandas
+pip install matplotlib
+pip install scipy
+pip install scikit-learn
+pip install requests
+pip install beautifulsoup4
+pip install flask
+pip install django
+
+echo Instalación completa.
+
+
+pause
--- Pyinstaller.bat
+++
@@ -1,1 +0,0 @@
-pyinstaller Fructuosando.py
