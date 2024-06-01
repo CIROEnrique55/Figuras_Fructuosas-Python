dd670c19689d5df3f58779056dbfa3869edf038f
CIROEnrique55 <ciroenrique4@hotmail.com>
2024-06-01 12:50:10 -0600

    Actualizacion del instalador.bat

--- Instalador.bat
+++ Instalador.bat
@@ -2,15 +2,16 @@
 echo Instalando bibliotecas de Python...
 echo 
 echo 
-echo   .oooooo.    o8o             .oooooo.    .oooooo..o                     o8o            
-echo  d8P'  `Y8b   `"'            d8P'  `Y8b  d8P'    `Y8                     `"'            
-echo 888          oooo  oooo d8b 888      888 Y88bo.       .ooooo.  oooo d8b oooo  oo.ooooo. 
-echo 888          `888  `888""8P 888      888  `"Y8888o.  d88' `"Y8 `888""8P `888   888' `88b
-echo 888           888   888     888      888      `"Y88b 888        888      888   888   888
-echo `88b    ooo   888   888     `88b    d88' oo     .d8P 888   .o8  888      888   888   888
-echo  `Y8bood8P'  o888o d888b     `Y8bood8P'  8""88888P'  `Y8bod8P' d888b    o888o  888bod8P'
-echo                                                                                888      
-echo                                                                               o888o        
+echo   .oooooo.    o8o             .oooooo.    .oooooo..o                     o8o                 .  
+echo  d8P'  `Y8b   `"'            d8P'  `Y8b  d8P'    `Y8                     `"'               .o8  
+echo 888          oooo  oooo d8b 888      888 Y88bo.       .ooooo.  oooo d8b oooo  oo.ooooo.  .o888oo
+echo 888          `888  `888""8P 888      888  `"Y8888o.  d88' `"Y8 `888""8P `888   888' `88b   888  
+echo 888           888   888     888      888      `"Y88b 888        888      888   888   888   888  
+echo `88b    ooo   888   888     `88b    d88' oo     .d8P 888   .o8  888      888   888   888   888 .
+echo  `Y8bood8P'  o888o d888b     `Y8bood8P'  8""88888P'  `Y8bod8P' d888b    o888o  888bod8P'   "888"
+echo                                                                                888              
+echo                                                                               o888o                   
+
 
 pip install --upgrade pip
 pip install pyinstaller
@@ -26,7 +27,7 @@ pip install beautifulsoup4
 pip install flask
 pip install django
 
-echo Instalación completa.
+echo Instalación completada.
 
 
 pause
